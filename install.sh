dabd428b4d2a96145dd06255dc3d2fd8cdfefcf0
Shynamo <broseau@gmail.com>
2019-07-25 15:52:42 +0900

    Fixed the install script

--- install.sh
+++ install.sh
@@ -19,19 +19,24 @@ rm -f $OPENCV_VERSION.zip
 # Compiling and installing OpenCV
 mkdir -p opencv-$OPENCV_VERSION/build
 cd opencv-$OPENCV_VERSION/build
-cmake -p -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_DIR $ROOT_DIR/opencv-$OPENCV_VERSION/
+cmake -p -DCMAKE_BUILD_TYPE=RELEASE \
+         -DCMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_DIR \
+         $ROOT_DIR/opencv-$OPENCV_VERSION/
 make -j4
 make -j4 install
 
 #=================== OPEN ALPR
 cd $ROOT_DIR
 
-## Getting OpenAlpr
+### Getting OpenAlpr
 git clone https://github.com/sunfic/openalpr-opencv4
 cd openalpr-opencv4/
 mkdir -p build
 cd build
-cmake -p -DOpenCV_FRAMEWORK_PATH=$OPENCV_INSTALL_DIR -DCMAKE_INSTALL_PREFIX=$OPENALPR_INSTALL_DIR ../src/
+cmake -p -DOpenCV_DIR=$OPENCV_INSTALL_DIR \
+         -DOpenCV_VERSION=$OPENCV_VERSION \
+         -DCMAKE_INSTALL_PREFIX=$OPENALPR_INSTALL_DIR \
+         ../src/
 make -j4
 make -j4 install
 
