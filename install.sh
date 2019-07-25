b0e69f838e4503f74a558d52f00751fe055921d6
Shynamo <broseau@gmail.com>
2019-07-25 15:25:36 +0900

    Fixed Doxyfile and modified Makefile

--- install.sh
+++ install.sh
@@ -1,48 +1,49 @@
 #TODO: Dependencies for OpenCV
 
-OPENCV_VERSION=3.4.5
+#Note: do not change anything
 ROOT_DIR=$(pwd)
+LPB_DIR=$ROOT_DIR/LPB
 
-# OPENCV
+OPENCV_VERSION=4.0.1
+OPENCV_INSTALL_DIR=$LPB_DIR/opencv     # Directory where OpenCV's librairies and headers will be installed
+OPENALPR_INSTALL_DIR=$LPB_DIR/openalpr # Directory where OpenALPR's librairies and headers will be installed
+
+#=================== OPENCV
+cd $ROOT_DIR
 
 ## Getting OpenCV from source
-#wget https://github.com/opencv/opencv/archive/$OPENCV_VERSION.zip
-#unzip $OPENCV_VERSION.zip
-#rm -f $OPENCV_VERSION.zip
-#
-## Compiling OpenCV
-#mkdir -p opencv-$OPENCV_VERSION/build_opencv
-#cd opencv-$OPENCV_VERSION/build_opencv
-#echo $ROOT_DIR/opencv-$OPENCV_VERSION/
-#cmake -p -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$ROOT_DIR/build_opencv/opencv3_install $ROOT_DIR/opencv-$OPENCV_VERSION/
-#make -j4
+wget https://github.com/opencv/opencv/archive/$OPENCV_VERSION.zip
+unzip $OPENCV_VERSION.zip
+rm -f $OPENCV_VERSION.zip
 
-## Retrieving librairies and includes
-#echo "Copying librairies and headers for opencv..."
-#cd $ROOT_DIR
-#mkdir -p  LPB/lib
-#mkdir -p  LPB/lib/opencv
-#mkdir -p  LPB/include
-#mkdir -p  LPB/include/opencv
-#cp opencv-$OPENCV_VERSION/build_opencv/lib/*.so opencv-$OPENCV_VERSION/build_opencv/lib/*.a LPB/lib/opencv/
-#cp -r opencv-$OPENCV_VERSION/modules/* LPB/include/opencv/
-#rm -f $(find LPB/include/opencv/ -type f -not -iname *.h*)
-#rm -rf $(find LPB/include/opencv/ -type d -empty)
+# Compiling and installing OpenCV
+mkdir -p opencv-$OPENCV_VERSION/build
+cd opencv-$OPENCV_VERSION/build
+cmake -p -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_DIR $ROOT_DIR/opencv-$OPENCV_VERSION/
+make -j4
+make -j4 install
 
-# OPEN ALPR
+#=================== OPEN ALPR
+cd $ROOT_DIR
 
 ## Getting OpenAlpr
-git clone https://github.com/openalpr/openalpr
-cd openalpr/
-mkdir -p build && cd build
-cmake ../src/ #TODO: Specifier le path to opencv
+git clone https://github.com/sunfic/openalpr-opencv4
+cd openalpr-opencv4/
+mkdir -p build
+cd build
+cmake -p -DOpenCV_FRAMEWORK_PATH=$OPENCV_INSTALL_DIR -DCMAKE_INSTALL_PREFIX=$OPENALPR_INSTALL_DIR ../src/
 make -j4
+make -j4 install
 
 
-## ## Retrieving librairies and includes
-echo "Copying librairies and headers for openalpr..."
-cd $ROOT_DIR
-mkdir -p  LPB/lib
-mkdir -p  LPB/lib/openalpr
-mkdir -p  LPB/include
-mkdir -p  LPB/include/openalpr
+#=================== $LPB_DIR
+cd $LPB_DIR
+
+## Building source
+make install
+
+# Testing on data
+echo "Testing..."
+(make test && echo "Testing success.\nInstallation complete.") || echo "Testing failed."
+#PATH=$PATH:$LPB_DIR/build/blur
+echo "Complete."
