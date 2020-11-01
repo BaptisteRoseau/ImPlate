28bafe6c45808bc821c68e0ca341ee9710aa67ca
Baptiste Roseau <broseau@gmail.com>
2020-11-01 12:18:43 +0100

    Changed 'blur_success.txt' to 'blur_success_gui.txt' for blur_gui executable

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd $(dirname $0) && rm -rf blur_failures.txt blur_success.txt log.txt data_in data_out
+cd $(dirname $0) && rm -rf blur_failures.txt blur_success.txt blur_failures_gui.txt blur_success_gui.txt log.txt data_in data_out
--- program/src/common/config.h
+++ program/src/common/config.h
@@ -1,20 +1,33 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-//TODO: Use .json config file
+#ifndef DFLT_OUTPUT_ADDON
 #define DFLT_OUTPUT_ADDON "_blurred"
+#endif
+#ifndef DFLT_JSON_ADDON
 #define DFLT_JSON_ADDON "_info"
+#endif
+#ifndef DFLT_BACKUP_ADDON
 #define DFLT_BACKUP_ADDON "_backup"
+#endif
+#ifndef DFLT_BLUR
 #define DFLT_BLUR 70
+#endif
+#ifndef DFLT_COUNTRY
 #define DFLT_COUNTRY "eu"
+#endif
 #ifndef DFLT_CONFIG_FILE
 #define DFLT_CONFIG_FILE "/usr/local/share/openalpr/config/openalpr.defaults.con"
 #endif
 #ifndef DFLT_RUNTIME_DIR
 #define DFLT_RUNTIME_DIR "/usr/local/share/openalpr/runtime_data/"
 #endif
+#ifndef DFLT_FAILURE_PIC_FILE
 #define DFLT_FAILURE_PIC_FILE "blur_failures.txt"
+#endif
+#ifndef DFLT_SUCCESS_PIC_FILE
 #define DFLT_SUCCESS_PIC_FILE "blur_success.txt"
+#endif
 #ifndef DFLT_MANUAL_BLUR_BIN
 #define DFLT_MANUAL_BLUR_BIN "./build/blur_ui"
 #endif
